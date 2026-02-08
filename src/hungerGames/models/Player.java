33f518bae7321da7931af737a233a8fde58dacf9
Santiago <luquevillanuevasantiago@gmail.com>
2026-02-08 22:48:31 +0100

    Hotfixes

--- src/hungerGames/models/Player.java
+++ src/hungerGames/models/Player.java
@@ -3,25 +3,19 @@ import hungerGames.strategys.Move;
 
 public abstract class Player {
     protected String name;
-    protected int vitality;
     protected String type;
+    protected int vitality;
     protected int x;
     protected int y;
     protected Move strategy;
 
-    public Player(String name, int vitality, String type, int x, int y, Move strategy){
+    public Player(String name, String type, int vitality, int x, int y, Move strategy){
         this.name = name;
-        this.vitality = vitality;
         this.type = type;
+        this.vitality = vitality;
         this.x = x;
         this.y = y;
         this.strategy = strategy;
-    }
-
-    public void performMove(Board board){
-        if (strategy != null) {
-            strategy.move(this, board);
-        }
     }
 
     public String getName() {
@@ -40,8 +34,20 @@ public abstract class Player {
         return y;
     }
 
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
     public void setPosition(int x, int y){
         this.x = x;
         this.y = y;
+    }
+
+    public void performMove(Board board){
+        this.strategy.move(this, board);
     }
 }
