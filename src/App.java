6316f07e71fda7ff5b27262029dca86c2c568794
Santiago <luquevillanuevasantiago@gmail.com>
2026-02-18 18:55:46 +0100

    Done Arreglos

--- src/App.java
+++ src/App.java
@@ -2,7 +2,7 @@ import hungerGames.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        HungerGames game = new HungerGames();
+        HungerGames game = new HungerGames(4, 4, 10); 
         game.start();
     }
 }
--- src/hungerGames/HungerGames.java
+++ src/hungerGames/HungerGames.java
@@ -1,151 +1,132 @@
 package hungerGames;
+
 import hungerGames.models.*;
 import hungerGames.strategys.*;
+import java.util.ArrayList; // Import for ArrayList
+import java.util.List;      // Import for List interface
 
 public class HungerGames {
     private Board board;
     
-    private Player hunter1;
-    private Player hunter2;
-    private Player hunter3;
-    private Player hunter4;
+    // --- LISTS (ArrayList) ---
+    private List<Player> hunters;
+    private List<Player> preys;
+    private List<Player> obstacles;
 
-    private Player prey1;
-    private Player prey2;
-    private Player prey3;
-    private Player prey4;
-
-    private Player obstacle1;
-    private Player obstacle2;
-    private Player obstacle3;
-    private Player obstacle4;
-
-    public HungerGames(){
+    // Constructor: receives the quantity of each type
+    public HungerGames(int huntersCount, int preysCount, int obstaclesCount){
         this.board = new Board(20, 20);
 
         Move hunterStrategy = new HunterMove();
         Move preyStrategy = new PreyMove();
 
-        this.hunter1 = new Hunter("H", (int)(Math.random() * 20), (int)(Math.random() * 20), hunterStrategy);
-        this.hunter2 = new Hunter("H", (int)(Math.random() * 20), (int)(Math.random() * 20), hunterStrategy);
-        this.hunter3 = new Hunter("H", (int)(Math.random() * 20), (int)(Math.random() * 20), hunterStrategy);
-        this.hunter4 = new Hunter("H", (int)(Math.random() * 20), (int)(Math.random() * 20), hunterStrategy);
+        // Initialize empty lists
+        this.hunters = new ArrayList<>();
+        this.preys = new ArrayList<>();
+        this.obstacles = new ArrayList<>();
 
-        this.prey1 = new Prey("P", (int)(Math.random() * 20), (int)(Math.random() * 20), preyStrategy);
-        this.prey2 = new Prey("P", (int)(Math.random() * 20), (int)(Math.random() * 20), preyStrategy);
-        this.prey3 = new Prey("P", (int)(Math.random() * 20), (int)(Math.random() * 20), preyStrategy);
-        this.prey4 = new Prey("P", (int)(Math.random() * 20), (int)(Math.random() * 20), preyStrategy);
+        // --- AUTOMATIC CREATION (Loops) ---
+        
+        // Create Hunters
+        for (int i = 0; i < huntersCount; i++) {
+            // We create the name "H" + number (e.g., H1, H2)
+            String name = "H"; 
+            Player h = new Hunter(name, (int)(Math.random() * 20), (int)(Math.random() * 20), hunterStrategy);
+            hunters.add(h);       // Add to list
+            board.placePlayer(h); // Add to board
+        }
 
-        this.obstacle1 = new Obstacle("#", (int)(Math.random() * 20), (int)(Math.random() * 20), null);
-        this.obstacle2 = new Obstacle("#", (int)(Math.random() * 20), (int)(Math.random() * 20), null);
-        this.obstacle3 = new Obstacle("#", (int)(Math.random() * 20), (int)(Math.random() * 20), null);
-        this.obstacle4 = new Obstacle("#", (int)(Math.random() * 20), (int)(Math.random() * 20), null);
+        // Create Preys
+        for (int i = 0; i < preysCount; i++) {
+            String name = "P";
+            Player p = new Prey(name, (int)(Math.random() * 20), (int)(Math.random() * 20), preyStrategy);
+            preys.add(p);
+            board.placePlayer(p);
+        }
 
-        board.placePlayer(hunter1);
-        board.placePlayer(hunter2);
-        board.placePlayer(hunter3);
-        board.placePlayer(hunter4);
-
-        board.placePlayer(prey1);
-        board.placePlayer(prey2);
-        board.placePlayer(prey3);
-        board.placePlayer(prey4);
-
-        board.placePlayer(obstacle1);
-        board.placePlayer(obstacle2);
-        board.placePlayer(obstacle3);
-        board.placePlayer(obstacle4);
+        // Create Obstacles
+        for (int i = 0; i < obstaclesCount; i++) {
+            Player obs = new Obstacle("#", (int)(Math.random() * 20), (int)(Math.random() * 20), null);
+            obstacles.add(obs);
+            board.placePlayer(obs);
+        }
     }
 
+    // Helper method to check if player is still on the board
     private boolean isAlive(Player p) {
-        return board.getGrid()[p.getY()][p.getX()] == p;
+        // Get whoever is currently on that board cell
+        Player onBoard = board.getGrid()[p.getY()][p.getX()];
+        // Check if it is the EXACT same object
+        return onBoard == p;
     }
 
-    public void start () throws Exception{
+    public void start() throws Exception {
         while (true) {
             board.cleanBoard(); 
             board.printBoard();
 
-            if (isAlive(hunter1)) {
-                hunter1.performMove(board);
-            }
-            if (isAlive(hunter2)){
-                hunter2.performMove(board);
-            }
-            if (isAlive(hunter3)){
-                hunter3.performMove(board);
-            }
-            if (isAlive(hunter4)){
-                hunter4.performMove(board);
+            // --- MOVEMENT LOOPS ---
+            
+            // Move Hunters
+            for (Player h : hunters) {
+                if (isAlive(h)) {
+                    h.performMove(board);
+                }
             }
 
-            if (isAlive(prey1)){
-                prey1.performMove(board);
-            }
-            if (isAlive(prey2)){
-                prey2.performMove(board);
-            }
-            if (isAlive(prey3)){
-                prey3.performMove(board);
-            }
-            if (isAlive(prey4)){
-                prey4.performMove(board);
+            // Move Preys
+            for (Player p : preys) {
+                if (isAlive(p)) {
+                    p.performMove(board);
+                }
             }
 
-            int huntersVivos = 0;
-            int preysVivos = 0;
+            // --- SURVIVOR COUNT ---
+            int huntersAlive = 0;
+            int preysAlive = 0;
 
-            // Contamos Hunters
-            if (isAlive(hunter1)){
-                huntersVivos++;
-            }
-            if (isAlive(hunter2)){
-                huntersVivos++;
-            }
-            if (isAlive(hunter3)){
-                huntersVivos++;
-            }
-            if (isAlive(hunter4)){
-                huntersVivos++;
+            // Count living Hunters
+            for (Player h : hunters) {
+                if (isAlive(h)) {
+                    huntersAlive++;
+                }
             }
 
-            // Contamos Preys
-            if (isAlive(prey1)){
-                preysVivos++;
-            }
-            if (isAlive(prey2)){
-                preysVivos++;
-            }
-            if (isAlive(prey3)){
-                preysVivos++;
-            }
-            if (isAlive(prey4)){
-                preysVivos++;
+            // Count living Preys
+            for (Player p : preys) {
+                if (isAlive(p)) {
+                    preysAlive++;
+                }
             }
 
-            if (preysVivos == 0) {
+            // --- WIN CONDITIONS ---
+            
+            // Case A: Hunters Win
+            if (preysAlive == 0) {
                 board.cleanBoard();
                 board.printBoard(); 
                 
                 System.out.println("\n===================================");
-                System.out.println("   ¡JUEGO TERMINADO!");
-                System.out.println("   Ganaron los: HUNTERS");
+                System.out.println("   GAME OVER!");
+                System.out.println("   Winners: HUNTERS");
                 System.out.println("===================================");
                 break;
             }
 
-            if (huntersVivos == 0) {
+            // Case B: Preys Win
+            if (huntersAlive == 0) {
                 board.cleanBoard();
                 board.printBoard();
                 
                 System.out.println("\n===================================");
-                System.out.println("   ¡JUEGO TERMINADO!");
-                System.out.println("   Ganaron los: PREYS");
+                System.out.println("   GAME OVER!");
+                System.out.println("   Winners: PREYS");
                 System.out.println("===================================");
                 break;
             }
 
-            System.out.println("Hunters vivos: " + huntersVivos + " | Preys vivas: " + preysVivos);
+            // Status update
+            System.out.println("Hunters Alive: " + huntersAlive + " | Preys Alive: " + preysAlive);
 
             Thread.sleep(400);
         }
